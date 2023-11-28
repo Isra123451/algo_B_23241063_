3df8b046c65d53e8b5e37df393a87ba0deb2e04f
Isra123451 <146052190+Isra123451@users.noreply.github.com>
2023-11-28 13:25:41 +0800

    Algo_28/11/2023

---
+++ 02-tipe data/src/App.java
@@ -0,0 +1,61 @@
+import java.util.function.DoubleToIntFunction;
+
+public class App  {
+    public static void main(String[] args) throws Exception {
+       //tipe data di java
+       //integer, byte, short, long, double, float, cher, booleam
+
+       //integer (satuan)
+       int i = 10;
+       system.out.println("=====INTEGER====");
+       System.out.println("nili integer i = "+  i);
+       System.out.println("nilai max : " + integer.MAX_VALUE);
+       System.out.println("nilai MIN : i" + integer.MIN_VALUE);
+       System.out.println("besar integer : " + integer.SIZE + "bit")ys;
+
+       //byte
+       byte b = 10;
+       System.out.println("=====BYTE====");
+       System.out.println("nilai byte b = " + b);
+       System.out.println("nilai MAX : " + byte .MAX_VALUE);
+       System.out.println("nilai MIN : " + byte .MIN_VALUE);
+       System.out.println("BESAR byte : " + byte .SIZE + "bit");  
+       
+       //short
+       short s= 10;
+       System.out.println("=====SHORT====");
+       System.out.println("nili short s = "+  s);
+       System.out.println("nilai MAX : " + short.MAX_VALUE);
+       System.out.println("nilai MIN : i" + short.MIN_VALUE);
+       System.out.println("besar byte : " + short.SIZE + "bit")ys;
+       
+       //long
+       long 1 = 10;
+        system.out.println("=====LONG====");
+       System.out.println("nili long 1 = "+  1);
+       System.out.println("nilai MAX : " + long .MAX_VALUE);
+       System.out.println("nilai MIN : " + long .MIN_VALUE);
+       System.out.println("besar long : " + long .SIZE + "bit");
+
+       //double
+       double d = 10;
+       System.out.println("=====DOUBLE====");
+       System.out.println("nili double d = "+  d);
+       System.out.println("nilai MAX : " + double .MAX_VALUE);
+       System.out.println("nilai MIN : " + double .MIN_VALUE);
+       System.out.println("besar double : " + Double .SIZE + "bit");
+
+       //char
+       char c = 10;
+       System.out.println("=====CHART====");
+       System.out.println("nili chart c = "+  c);
+       System.out.println("besar chart : " + char .SIZE + "bit");
+
+       //boolean
+       boolean val = false;
+       System.out.println("=====BOOLEAN====");
+       System.out.println("nili boolean val = "+  val);
+       System.out.println("besar boolean : " + boolean .SIZE + "bit");
+       
+    }
+}
